c900c15eafe8d582ca740385ec7846f8cc32f56c
MOHAN-K2004 <727821tucs129@skct.edu.in>
2023-10-05 10:25:02 +0530

    Day15

---
+++ Day 14/src/main/java/com/example/demo/service/ContractService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.ContractModel;
+import com.example.demo.repository.ContractRepository;
+
+@Service
+public class ContractService {
+    
+    @Autowired
+    private ContractRepository contractRepository;
+
+    public ArrayList<ContractModel> getAllContracts() {
+        return null;
+    }
+
+}
